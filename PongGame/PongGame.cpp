5cb3d611433b07443b0bd346ed4b443bac8908ec
SculptTechProject <sculpttechproject@gmail.com>
2024-08-24 00:03:23 +0200

    Add project files.

---
+++ PongGame/PongGame.cpp
@@ -0,0 +1,167 @@
+
+#include <iostream>
+#include <raylib.h>
+
+using namespace std;
+
+int player_score = 0;
+int cpu_score = 0;
+
+class Ball {
+public:
+	float x, y;
+	int speed_x, speed_y;
+	int radius;
+
+	void Draw() {
+		DrawCircle(x, y, radius, WHITE);
+	}
+
+	void Update() {
+		x += speed_x;
+		y += speed_y;
+
+		if (y + radius >= GetScreenHeight() || y - radius <= 0) {
+			speed_y *= -1;
+		}
+		if (x + radius >= GetScreenWidth()) {
+			cpu_score++;
+			ResetBall();
+			WaitTime(0.5);
+		}
+
+		if (x - radius <= 0) {
+			player_score++;
+			ResetBall();
+			WaitTime(0.5);
+		}
+	}
+
+	void ResetBall() {
+		x = GetScreenWidth() / 2;
+		y = GetScreenHeight() / 2;
+
+		int speed_choices[2] = { -1,1 };
+		speed_x *= speed_choices[GetRandomValue(0, 1)];
+		speed_y *= speed_choices[GetRandomValue(0, 1)];
+	}
+};
+
+class Paddle {
+protected:
+	void LimitMovement() {
+		if (y <= 0) {
+			y = 0;
+		}
+		if (y + height >= GetScreenHeight()) {
+			y = GetScreenHeight() - height;
+		}
+	}
+
+public:
+	float x, y;
+	float width, height;
+	int speed;
+	Color color;
+
+	void Draw() {
+		DrawRectangle(x, y, width, height, color);
+	}
+
+	void Update() {
+		if (IsKeyDown(KEY_UP)) {
+			y = y - speed;
+		}
+		if (IsKeyDown(KEY_DOWN)) {
+			y = y + speed;
+		}
+		LimitMovement();
+	}
+};
+
+class CpuPaddle : public Paddle {
+public:
+	void Update(int ball_y) {
+		if (y + height / 2 > ball_y) {
+			y = y - speed;
+		}
+		if (y + height / 2 <= ball_y) {
+			y = y + speed;
+		}
+		LimitMovement();
+	}
+};
+
+Ball ball;
+Paddle player;
+CpuPaddle cpu;
+
+int main() {
+	cout << "Starting the game!" << endl;
+
+	const int screen_width = 1280;
+	const int screen_height = 800;
+
+	InitWindow(screen_width, screen_height, "Pong Game!");
+	SetTargetFPS(60);
+
+	int speedB = 8; //Speed of ball
+
+	//ball
+	ball.radius = 20;
+	ball.x = screen_width / 2;
+	ball.y = screen_height / 2;
+	ball.speed_x = speedB;
+	ball.speed_y = speedB;
+
+	int speedP = 6;  //Speed of player and cpu
+
+	//player
+	player.width = 25;
+	player.height = 120;
+	player.x = screen_width - player.width - 10;
+	player.y = screen_height / 2 - player.height / 2;
+	player.speed = speedP;
+	player.color = YELLOW;
+
+	//cpu
+	cpu.height = 120;
+	cpu.width = 25;
+	cpu.x = 10;
+	cpu.y = screen_height / 2 - cpu.height / 2;
+	cpu.speed = speedP;
+	cpu.color = RED;
+
+	while (WindowShouldClose() == false) {
+		BeginDrawing();
+
+		//updating
+		ball.Update();
+		player.Update();
+		cpu.Update(ball.y);
+
+		//checking for collisions
+		if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius, Rectangle{ player.x, player.y, player.width, player.height })) {
+			ball.speed_x *= -1;
+		}
+		if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius, Rectangle{ cpu.x, cpu.y, cpu.width, cpu.height })) {
+			ball.speed_x *= -1;
+		}
+
+		// drawing
+		ClearBackground(BLACK);
+		DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+		ball.Draw();
+		cpu.Draw();
+		player.Draw();
+
+		DrawText(TextFormat("%i", cpu_score), screen_width / 4 - 20, 20, 80, WHITE);
+		DrawText(TextFormat("%i", player_score), 3 * screen_width / 4 - 20, 20, 80, WHITE);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return 0;
+}
